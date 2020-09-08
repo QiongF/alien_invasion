c7365fe5b3de5a60c0ca9ad3fa2fc81d0272ef2c
QiongF <fanqiong27@gmail.com>
2020-09-08 20:43:49 +0200

    created the very 1st version of alien_invasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,22 @@
+import sys
+
+import pygame
+
+def run_game():
+    # Initialize pygame and create a screen object.
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Start the main loop for the game.
+    while True:
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Visalize the latest screen
+        pygame.display.flip()
+
+run_game()
+
